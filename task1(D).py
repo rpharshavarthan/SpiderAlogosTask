b0390aca7622d900249a1f2a36d60d6253c1ba63
harsha varthan <66173033+rpharshavarthan@users.noreply.github.com>
2020-08-19 21:08:35 +0530

    Add files via upload

--- task1(D).py
+++ task1(D).py
@@ -1,13 +1,15 @@
-#TASk-1(D)-DETERMINE FINAL RATING
+#TASk-1(D)
+#adit can perform well if contest happens(1) and he eats SCN(1) on the day of contest
+#he cannot perform well if contest happens(1) and he doesn't eats SCN(0) on the day of contest
 
 n, r, x, y = map(int, input().split())
 newr = r
 c = list(map(int, input().split()))
 s = list(map(int, input().split()))
 for i in range(n):
-    if c[i] == 1 and s[i] == 1:
+    if c[i] == 1 and s[i] == 1:   #performs Well
         newr += x
-    elif c[i] == 1 and s[i] == 0:
+    elif c[i] == 1 and s[i] == 0:   #doesn't performs well
         newr -= y
 if newr > r:
     print("promoted")
